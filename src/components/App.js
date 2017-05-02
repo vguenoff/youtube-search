67f372f9111e12737cfa6278a5d5f0721cabd3b8
vguenoff <vgenoff@gmail.com>
2017-05-02 21:08:45 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const ApiKey = 'AIzaSyCNR2v1-oICLywkOdbNvcsKNd7HdQYL0ZI';
+
+const App = () => (
+  <div>
+    <SearchBar />
+  </div>
+);
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => (
+  <input type="text" />
+);
+
+export default SearchBar;
